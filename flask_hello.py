194c603edc3f9e0b05bc7e902e5177189f88e8d8
lyx_homepc <358225427@qq.com>
2020-05-22 23:06:39 +0800

    --m

--- flask_hello.py
+++ flask_hello.py
@@ -1,11 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    url_str = 'www.baidu.com'
+
+    my_dict = {
+        'name': '狗崽子',
+        'age': 19
+    }
+
+    my_list = [1, 3, 5, 7, 9]
+    return render_template('index.html',
+                           url_str=url_str,
+                           my_list=my_list,
+                           my_dict=my_dict)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
